8350f1f01b514ff9dca66f61cdd5fb0cf3d1a973
flymwy <flymwy@gmail.com>
2012-09-12 16:47:18 +0000

    修改某些库的单例调用 修复一些BUG 增加readonly判断，只要存在非select语句，程序就会报错并退出

--- gilFramework/gilMysql.php
+++ gilFramework/gilMysql.php
@@ -1,18 +1,37 @@
 <?php
 class gilMysql{
 	
+	static public $_db = null;
+	
+	static public $_cursor = null;
+	
 	static public $usedTime = 0;
+	
+	private function __construct(){
+		global $gilConfig;
+		if($gilConfig['db_config']['pconnect']) self::$_db = mysql_pconnect($gilConfig['db_config']['host'],$gilConfig['db_config']['user'],$gilConfig['db_config']['password']);
+		else self::$_db = mysql_connect($gilConfig['db_config']['host'],$gilConfig['db_config']['user'],$gilConfig['db_config']['password']);
+		mysql_select_db($gilConfig['db_config']['dbname'],self::$_db);
+		mysql_query("set names ".$gilConfig['db_config']['charset'],self::$_db);
+	}
+	
+	static public function _disconn(){
+		mysql_close(self::$_db);
+		self::$_db = null;
+		self::$_cursor = null;
+	} 
 	
 	/**
 	 * 连接
 	 * @param array $gilConfig
 	 * @return object
 	 */
-	static public function _conn($gilConfig){
-		$db = mysql_connect($gilConfig['db_config']['host'],$gilConfig['db_config']['user'],$gilConfig['db_config']['password']);
-		mysql_select_db($gilConfig['db_config']['dbname'],$db);
-		mysql_query("set names ".$gilConfig['db_config']['charset'],$db);
-		return $db;
+	static public function _conn(){
+		if(self::$_cursor === null){
+			$c = __CLASS__ ;  
+            self::$_cursor = new $c();  
+		}
+		return self::$_cursor;
 	}
 	
 	/**
@@ -21,9 +40,10 @@ class gilMysql{
 	 * @param object $db
 	 * @return object
 	 */
-	static public function exec($sql, $db = null){
+	static public function exec($sql){
+		$sql = self::check($sql);
 		$usedTimeStart = microtime();//检测SQL请求耗时，探针
-		$exec = mysql_query($sql, $db);
+		$exec = mysql_query($sql, self::$_db);
 		self::$usedTime = microtime() - $usedTimeStart;//检测SQL请求耗时，存入探针
 		return $exec;
 	}
@@ -34,13 +54,28 @@ class gilMysql{
 	 * @param object $db
 	 * @return 二维数组
 	 */
-	static public function getArray($sql, $db = null){
-		if(! $result = self::exec($sql, $db) )	return array();
+	static public function getArray($sql){
+		if(! $result = self::exec($sql, self::$_db) )	return array();
 		if(! mysql_num_rows($result) )		return array();
 		$rows = array();
 		while ($row = mysql_fetch_array($result,MYSQL_ASSOC))
 			$rows[] = $row;
 		mysql_free_result($result);
 		return $rows;
+	}
+	
+	/**
+	 * 检测SQL是否合法并过滤非法值
+	 * @param string $sql
+	 * @return string
+	 */
+	static public function check($sql){
+		global $gilConfig;
+		if($gilConfig['db_readonly'] == true){
+			$sql=str_ireplace(array('INSERT','UPDATE','DELETE','FILE','CREATE','ALTER','INDEX','DROP','CREATETEMPORARYTABLES','SHOWVIEW','CREATEROUTINE','ALTERROUTINE','EXECUTE','CREATEVIEW','EVENT','TRIGGER','GRANT','SUPER','PROCESS','RELOAD','SHUTDOWN','SHOWDATABASES','LOCKTABLES','REFERENCES','EPLICATIONCLIENT','REPLICATIONSLAVE','CREATEUSER'),'',$sql,$i);
+			if($i>0) exit;
+		}
+		$sql = mysql_real_escape_string($sql,self::$_db);
+		return $sql;
 	}
 }
--- test.php
+++ test.php
@@ -3,11 +3,15 @@ define('GILPATH',dirname(__FILE__).'/gilFramework');
 include(GILPATH.'/gilCore.php');
 
 //$result = gilDB::findAll('select * from test');
+
+
 //print_r($result);
 
 //gilCache::set('1', 'nihso', '123');exit;
-/*
+
 gilDB::select('test',null,null,'0,1','id as aid,name as aname');
-gilDB::join('b',array('test.id'=>'b.bid'));
-$result = gilDB::find();
-print_r($result);print_r(gilMysql::$usedTime);*/
+//gilDB::join('b',array('test.id'=>'b.bid'));
+//gilDB::findSql('delete from test');
+$result = gilDB::findAll();
+print_r($result);
+print_r(gilMysql::$usedTime);
