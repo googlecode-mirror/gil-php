afc6a9d16b13ef4529352388a5e4b7df944f7f72
flymwy@gmail.com <flymwy@gmail.com@57b2b0b0-ed00-cb1a-730b-9e0fab1ff5f8>
2012-09-14 16:03:33 +0000

    完成demo文件的注释工作

--- trunk/index.php
+++ trunk/index.php
@@ -7,5 +7,11 @@ define('APPMODELPATH',dirname(__FILE__).'/protected/model');
 define('APPVIEWPATH',dirname(__FILE__).'/protected/view');
 */
 
+//URL的访问标准是 http://xxxx/index.php?r=main/index
+//默认会执行controller文件夹下的MainController.php
+//然后执行该控制器类下的actionIndex()
+//返回一个数组，然后程序会载入view文件夹下的main/index.php
+//PS：程序不会自动调用model，你需要显式声明调用，程序会搜索model文件夹下相应model.php及class model
+
 include('gilFramework/GilCore.php'); //只需一条语句即可使用OO编程
 ?>
